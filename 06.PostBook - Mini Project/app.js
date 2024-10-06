cde5a4be94a245e3153660767ffc827201674dfd
Pushp Raj <pushprajsunshin@gmail.com>
2024-10-06 18:27:17 +0530

    mini project

---
+++ 06.PostBook - Mini Project/app.js
@@ -0,0 +1,127 @@
+const express = require('express')
+const app = express()
+const userModel = require("./models/user")
+const postModel = require("./models/post")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+var cookieParser = require('cookie-parser')
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/register', async (req, res) => {
+  let { name, age, username, email, password } = req.body
+
+  let user = await userModel.findOne({ email })
+  if (user)
+    return res.status(500).send("User already registered")
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      // Stores hash password and other data in DB.
+      let user = await userModel.create({
+        name,
+        age,
+        username,
+        email,
+        password: hash
+      })
+
+      let token = jwt.sign({ email: user.email, userid: user._id }, "shhhhsecrethai")
+      res.cookie("token", token)
+      res.send("Registered successfully")
+    })
+  })
+
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+  let { email, password } = req.body
+
+  let user = await userModel.findOne({ email })
+  if (!user)
+    return res.status(500).send("Something is wrong")
+
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) {
+      let token = jwt.sign({ email: user.email, userid: user._id }, "shhhhsecrethai")
+      res.cookie("token", token)
+      res.status(200).redirect("/profile")
+    }
+    else {
+      res.redirect("/login")
+    }
+  })
+
+})
+
+app.get('/profile', isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email }).populate("posts")
+  res.render("profile", { user })
+})
+
+app.get('/like/:id', isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user")
+
+  if (post.likes.indexOf(req.user.userid) === -1) {
+    post.likes.push(req.user.userid)
+  } else {
+    post.likes.splice(post.likes.indexOf(req.user.userid), 1)
+  }
+  await post.save()
+  res.redirect("/profile")
+})
+
+app.get('/edit/:id', isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user")
+  let user = await userModel.findOne({ email: req.user.email })
+  res.render("edit", {user, post})
+})
+
+app.post('/update/:id', isLoggedIn, async (req, res) => {
+  let post = await postModel.findOneAndUpdate({ _id: req.params.id }, {content: req.body.content})
+  res.redirect("/profile")
+})
+
+app.post('/post', isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email })
+  let { content } = req.body
+
+  let post = await postModel.create({
+    user: user._id,
+    content
+  })
+  user.posts.push(post._id)
+  await user.save()
+  res.redirect("/profile")
+})
+
+app.get('/logout', (req, res) => {
+  res.cookie("token", "")
+  res.redirect('/login')
+})
+
+function isLoggedIn(req, res, next) {
+  if (req.cookies.token === "")
+    res.redirect("/login")
+
+  else {
+    let data = jwt.verify(req.cookies.token, "shhhhsecrethai")
+    req.user = data
+    next()
+  }
+}
+
+app.listen(3000, () => {
+  console.log('This app is listening on port 3000!')
+})
